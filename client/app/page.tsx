688ab8460d9f9de8d6ce4a96a4d545106c7bc24e
Martin Cain <martincain001@gmail.com>
2023-08-15 22:24:26 -0400

    home gutter test

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,7 +3,7 @@ import GutteredWide from './_layouts/GutteredWide'
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between p-24">
+    <main>
       <GutteredWide>
         <SessionDetails />
       </GutteredWide>
