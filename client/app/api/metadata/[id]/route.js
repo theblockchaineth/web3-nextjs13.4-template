75a9ed6999922e991540290b49388b45d8c8f934
Martin Cain <martincain001@gmail.com>
2023-08-25 11:46:56 -0400

    log reader pipeline & metadata api

---
+++ client/app/api/metadata/[id]/route.js
@@ -0,0 +1,14 @@
+import { NextResponse } from 'next/server';
+import path from 'path';
+import { promises as fs } from 'fs';
+
+export async function GET(req, { params }) {
+
+    const { id } = params
+    const jsonDirectory = path.join(process.cwd(), 'app/api/metadata/[id]');
+    const fileContents = await fs.readFile(jsonDirectory + '/data.json', 'utf8');
+
+    console.log(id, jsonDirectory, fileContents)
+
+    return NextResponse.json({ id })
+}
